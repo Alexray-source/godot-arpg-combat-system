2febef3795752c363385479a0ae88b7cd6f45348
Alexray <52537531+Alexray-source@users.noreply.github.com>
2026-05-27 19:39:16 +0200

    Ricochet bullet changes

--- Scripts/Node/Projectiles/ricochet_bullet.gd
+++ Scripts/Node/Projectiles/ricochet_bullet.gd
@@ -9,6 +9,7 @@ class_name RicochetProjectile extends Projectile
 var is_ricoshot : bool = false
 var blacklist : Array[HurtBox]
 var _scan_shape : SphereShape3D
+var target_characters : bool = false
 
 func _ready() -> void:
 	_scan_shape = SphereShape3D.new()
@@ -22,6 +23,13 @@ func _ready() -> void:
 		#set_physics_process(false)
 		#get_tree().create_timer(ricochet_delay).timeout.connect(set_physics_process.bind(true))
 
+func filter_for_character(hurtbox : HurtBox):
+	return hurtbox.get_parent_node_3d() != null and hurtbox.get_parent_node_3d() is CombatCharacter
+
+func filter_hurtbox_is_not_character(hurtbox : HurtBox):
+	return hurtbox.get_parent_node_3d() != null and not hurtbox.get_parent_node_3d() is CombatCharacter
+
+
 func ricochet(intersected_hurtbox : HurtBox):
 	if allowed_ricoshots == 0:
 		return
@@ -31,16 +39,26 @@ func ricochet(intersected_hurtbox : HurtBox):
 	
 	var hurtbox_scanner : HurtBoxScanner = HurtBoxScanner.new()
 	hurtbox_scanner.blacklist = scan_blacklist
-	var closest_hurtbox : HurtBox = hurtbox_scanner.get_closest_hurtbox_to_position(intersected_hurtbox.global_position, get_world_3d().direct_space_state, _scan_shape, intersected_hurtbox.global_transform.orthonormalized(), 2 + owning_layer.get_enemy_layer())
+	#var closest_hurtbox : HurtBox = hurtbox_scanner.get_closest_hurtbox_to_position(intersected_hurtbox.global_position, get_world_3d().direct_space_state, _scan_shape, intersected_hurtbox.global_transform.orthonormalized(), 2 + owning_layer.get_enemy_layer())
+
+	var nearby_hurtboxes : Array[HurtBox] = hurtbox_scanner.get_nearby_hurtboxes_from_position(get_world_3d().direct_space_state, _scan_shape, intersected_hurtbox.global_transform.orthonormalized(), 2 + owning_layer.get_enemy_layer())
 	
-	if closest_hurtbox == null:
+	#if closest_hurtbox == null:
+		#return
+		
+	if nearby_hurtboxes.size() == 0:
+		return
+		
+	if target_characters == true:
+		nearby_hurtboxes = nearby_hurtboxes.filter(filter_for_character)
+	else:
+		nearby_hurtboxes = nearby_hurtboxes.filter(filter_hurtbox_is_not_character)
+	
+	if nearby_hurtboxes.size() == 0:
 		return
 	
-	#if nearby_hurtboxes.size() == 0:
-		#return
-	#
-	#var random_hurtbox = nearby_hurtboxes.pick_random()
-	var direction = intersected_hurtbox.global_position.direction_to(closest_hurtbox.global_position)
+	var random_hurtbox = nearby_hurtboxes.pick_random()
+	var direction = intersected_hurtbox.global_position.direction_to(random_hurtbox.global_position)
 	
 	var new_bullet : RicochetProjectile = load(scene_file_path).instantiate()
 	new_bullet.allowed_ricoshots = allowed_ricoshots - 1
@@ -51,6 +69,7 @@ func ricochet(intersected_hurtbox : HurtBox):
 	new_bullet.atk_info = atk_info
 	new_bullet.max_lifetime = max_lifetime
 	new_bullet.speed = ricochet_speed
+	new_bullet.target_characters = not target_characters
 	
 	get_tree().current_scene.add_child(new_bullet)
 	new_bullet.global_basis = Basis.looking_at(direction)
